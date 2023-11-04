f418e232c1fea9ef6dad628de6752f0690642d06
Abril <abrilv29@gmail.com>
2023-11-04 13:19:30 -0600

    Cambios en el css de componet-login

--- burquer-queen/src/app/app-routing.module.ts
+++ burquer-queen/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login-component/login/login.component';
+import { MeseroPedidosComponent } from './mesero-component/mesero-pedidos/mesero-pedidos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:LoginComponent },
+  { path: 'mesero-pedidos', component:MeseroPedidosComponent},
+  { path: '**', redirectTo: '',pathMatch:'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
